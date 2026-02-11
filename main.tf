5e4d4787d041aec4fcacf2bef41741f2e9d428e9
prabhaIT <drapj1227@gmail.com>
2026-02-11 22:11:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 resource "aws_instance" "dev" {
     ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     tags = {
-      Name = "vvvnaresh"
+      Name = "prabha"
     }
 }
